1c45c2b9ce6bc66a21721c5f5c92a0059fc37234
codelily <coldelily98@naver.com>
2024-11-15 14:33:20 +0900

    Jenkins Update SpringBoot&React

---
+++ src/main/frontreact/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP</h1>
+      <h3>카운트 : 1</h3>
+      <img src="./image/북극여우.jpg" alt="북극여우" />
+    </div>
+  );
+};
+
+export default App;
